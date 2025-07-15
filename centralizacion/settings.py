7c462b55c7a397565a321c4c003ae9d003698940
Alvaro Encinas <AlvaroEncinas26@gmail.com>
2025-07-15 13:01:29 -0400

    V2

--- centralizacion/settings.py
+++ centralizacion/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4^37a#feuq%t$lcg1erz2aml4koeeni9fww7yne1(!w*i$vmj9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
