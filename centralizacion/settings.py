47a68480944e9a87f0a34cb8f4edda5bd6d04e4d
Alvaro Encinas <AlvaroEncinas26@gmail.com>
2025-08-19 20:08:16 -0400

    static

--- centralizacion/settings.py
+++ centralizacion/settings.py
@@ -14,7 +14,16 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+import os
 
+# Configuración para archivos estáticos
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Si tienes archivos estáticos locales
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
